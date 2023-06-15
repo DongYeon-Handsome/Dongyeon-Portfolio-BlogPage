d6eeb890f7d7ea671f95f780ff280d9a5d85ca68
Dongyeon-Handsome <dongyeon4367@gmail.com>
2023-06-16 05:50:23 +0900

    fix: 경로 주소 변경

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,19 +2,17 @@ import React from 'react';
 import { useSetRecoilState } from 'recoil';
 import { menuAtom } from '../recoil/atom';
 import MainTop from '../components/main/MainTop';
-import TectStat from '../components/main/TectStat';
-import ProjectSlide from '../components/main/ProjectSlide';
-import AboutMe from '../components/main/AboutMe';
+import Project from './Project';
+import AboutMe from './AboutMe';
 
 export default function Main(){
-    const setMenu = useSetRecoilState(menuAtom);
-    return(
-      <div style={{ marginTop: '6rem' }}>
-        {setMenu(1)}
-            <MainTop />
-            <AboutMe />
-            <TectStat />
-            <ProjectSlide />
-      </div>  
-    );
+  const setMenu = useSetRecoilState(menuAtom);
+  return(
+    <div style={{ marginTop: '6rem' }}>
+      {setMenu(1)}
+        <MainTop />
+        <AboutMe />
+        <Project />
+    </div>  
+  );
 }
