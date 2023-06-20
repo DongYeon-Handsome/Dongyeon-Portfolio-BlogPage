8816facf0c79d6c72fc4880dfd8cd8c3e5962786
Dongyeon-Handsome <dongyeon4367@gmail.com>
2023-06-21 08:12:07 +0900

    feat: 파일 삭제

--- src/components/Carousel.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-
-export default function Carousel({ ...settings }) {
-    return <Slider {...settings}></Slider>;
-}
