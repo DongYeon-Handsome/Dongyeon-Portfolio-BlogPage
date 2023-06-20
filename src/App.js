72fea6bfb150e1ff387fe65be16f5eadf52b64a5
Dongyeon-Handsome <dongyeon4367@gmail.com>
2023-06-21 08:11:23 +0900

    feat: 코드 간결화

--- src/App.js
+++ src/App.js
@@ -1,36 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
-import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/GlobalStyle';
 import Main from './pages/Main';
-import Project from './pages/Project';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import ScrollToTop from './components/ScrollToTop';
-
-const theme = {
-  primaryColor: '#01base',
-  secondaryColor: '#000',
-  TitleFont: 'TimeNewRomanPSMT',
-};
 
 function App() {
   return (
     <div className="App">
       <RecoilRoot>
-        <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          <BrowserRouter>
-            <ScrollToTop />
-            <Header />
-            <Routes>
-              <Route exact path="/" element={<Main />}></Route>
-              <Route exact path="/Project" element={<Project />}></Route>
-            </Routes>
-            <Footer />
-          </BrowserRouter>
-        </ThemeProvider>
+        <GlobalStyle />
+        <Header />
+        <Main />
+        <Footer />
       </RecoilRoot>
     </div>
   );
